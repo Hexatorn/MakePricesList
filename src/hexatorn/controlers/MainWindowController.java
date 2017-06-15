e2e4c1a9d10e784d6816945d2445d68ac00600a3
Hexatorn <pawel.niemczak@gmail.com>
2017-06-15 19:28:22 +0200

    Odczyt konfiguracji z XML Stworzenie GUI i obsługa przycisku "+"

--- src/hexatorn/controlers/MainWindowController.java
+++ src/hexatorn/controlers/MainWindowController.java
@@ -1,9 +1,12 @@
 package hexatorn.controlers;
-
 import hexatorn.util.ReadConfigFromXML;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ToolBar;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import java.util.ArrayList;
 
 public class MainWindowController {
 
@@ -15,10 +18,86 @@ public class MainWindowController {
     private ToolBar toolBarMenu;
 
     @FXML
-    public void initialize() {
-        ReadConfigFromXML.getProfileName();
+    private BorderPane centerPane;
+    @FXML
+    private BorderPane mainPane;
+    @FXML
+    private BorderPane menuPane;
+    @FXML
+    private VBox vBoxColumnId;
+    @FXML
+    private VBox vBoxOutColumnName;
+    @FXML
+    private VBox vBoxInColumnName;
+    @FXML
+    private ScrollPane scrolPane;
 
-        toolBarMenu.getItems().addAll(new Button("test"));
-        //System.out.println(toolBarMenu.getItems());
+
+    @FXML
+    private BorderPane filePane1;
+
+    /**Test**/
+    private Button button1 = new Button("test");
+
+    public void y(){
+        button1.setOnAction(event -> x());
+    }
+
+    private void x(){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Warning Alert");
+        alert.setHeaderText("Błąd odczytu pliku konfiguracyjknego.");
+        alert.showAndWait();
+    }
+    /**Koniec Testu**/
+
+
+    @FXML
+    private VBox vBoxtest;
+
+    @FXML
+    public void onActionAddNewField(){
+        int i = vBoxColumnId.getChildren().size();
+        Label lbl = new Label(""+i);
+        lbl.setPrefHeight(25);
+        vBoxColumnId.getChildren().add(lbl);
+        vBoxOutColumnName.getChildren().add(new TextField());
+        vBoxInColumnName.getChildren().add(new TextField());
+    }
+
+    public void initialize() {
+        createMenu();
+        toolBarMenu.getItems().add(button1);
+    }
+
+    private void createMenu() {
+        ArrayList<String> btnName = ReadConfigFromXML.getProfileName();
+        for (String s :btnName
+             ) {
+            Button btn = new Button("Cennik "+s);
+            btn.setOnAction(event -> x());
+
+            toolBarMenu.getItems().add(btn);
+
+
+        }
+        Object[] objects = toolBarMenu.getItems().toArray();
+        double with = 90;
+        for (Object o : objects) {
+            String tmpstr = ((Button) o).getText();
+            int tmp = tmpstr.length();
+            if(tmp*6>with)
+                with = tmp*7;
+        }
+        for (Object o : objects) {
+            ((Button) o).setMinWidth(with);
+            ((Button) o).setMaxWidth(with);
+            toolBarMenu.setMinWidth(with+10);
+            toolBarMenu.setMaxWidth(with+10);
+
+        }
+        Node node = toolBarMenu.getItems().get(0);
+        toolBarMenu.getItems().remove(0);
+        toolBarMenu.getItems().add(node);
     }
 }
--- src/hexatorn/util/ReadConfigFromXML.java
+++ src/hexatorn/util/ReadConfigFromXML.java
@@ -3,11 +3,10 @@ package hexatorn.util;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.ArrayList;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
-
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import org.w3c.dom.Document;
@@ -16,36 +15,38 @@ import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
-import java.io.File;
 
 /**
  * Created by Hexatorn on 2017-06-12.
  */
 public class ReadConfigFromXML {
 
-    public static void getProfileName(){
+    private static Document doc = null;
+
+    public static ArrayList<String> getProfileName(){
+        ArrayList<String> arrayList = new ArrayList<>();
+        if (doc==null)
+            readXMLFile();
+        NodeList nodeList = doc.getElementsByTagName("Profile");
+
+        for (int i = 0; i < nodeList.getLength(); i++){
+            Node node = nodeList.item(i);
+            Element element = ((Element) node);
+            arrayList.add(element.getElementsByTagName("ProfileName").item(0).getTextContent());
+        }
+        return arrayList;
 
     }
 
     private static void readXMLFile(){
-        System.out.println("Wczytywanie pliku konfiguracyjnego");
         File file = new File(System.getProperty("user.home")+"\\AppData\\Roaming\\Duka Make Price List\\Config.xml");
 
 
         try{
-//            Scanner in = new Scanner(file);
-//
-//            while (in.hasNext()){
-//                System.out.println(in.nextLine());
-//            }
-
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(file);
+            doc = dBuilder.parse(file);
             doc.getDocumentElement().normalize();
-
-            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-
         }
         catch(FileNotFoundException e){
             Alert alert = new Alert(AlertType.WARNING);
@@ -57,13 +58,12 @@ public class ReadConfigFromXML {
                     "2. Przekopiowanie pliku konfiguracyjnego do z innego komputera.\n"+
                     "3. Stworzenie nowej konfiguracji w programie i zapisanie.");
             alert.showAndWait();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        } catch (ParserConfigurationException | IOException e) {
             Alert alert = new Alert(AlertType.WARNING);
             alert.setTitle("Warning Alert");
             alert.setHeaderText("Błąd odczytu pliku konfiguracyjknego.");
             alert.setContentText(""+e.getMessage());
+            alert.showAndWait();
             alert.showAndWait();
             e.printStackTrace();
         } catch (SAXException e) {
